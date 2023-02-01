af3bbf1b37e560dc240881a19dc4ececfcb23751
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-02-01 00:13:49 -0600

    Read multiple lines at a time

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import requests
-from requests import Response
+#import requests
+#from requests import Response
+from itertools import islice
 
-url = ['https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods', 
-    'https://www.oreilly.com/library/view/python-in-a/0596001886/ch04s09.html', 
-    'https://www.google.com/docs/abou'] #Last Url should give you a 404 error
+#index = 0
+with open('url.txt','r') as file:
+    while True:
+        lines = list(islice(file, 1000))
+        for line in lines:
+            #index += 1
+            print(line)
+        if not lines:
+            break
 
-r: Response = requests.get(url)
-print(f"""
-DATE = {r.headers['Date']}  
-Content-Type = {r.headers['Content-Type']}
-""")
 
-print(f"Status Code: {r.status_code}\n") 
 
-#Working on for loop to use with if statements
-#Take from list of urls
-#for url:
-
-if r.status_code == 200: 
-    print("The URL is OK")
-elif r.status_code == 404:
-    print("The URL is not found")
-elif r.status_code == 502:
-    print("Bad Gateway")
 
