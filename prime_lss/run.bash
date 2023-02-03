2efa041d4b6f9e40e2d00d59750ea16470d54408
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-02-03 16:00:00 -0600

    Remove qualifier

--- prime_lss/run.bash
+++ prime_lss/run.bash
@@ -5,6 +5,6 @@ split -l 10000 mergedURLs.txt
 mkdir splits json
 mv x* splits/
 
-for $i in $(ls splits); do mkdir json/$i; done
+for i in $(ls splits); do mkdir json/$i; done
 
-for $i in $(ls splits); do python __main__.py -i $i -o json/$1; done
+for i in $(ls splits); do python __main__.py -i $i -o json/$1; done
